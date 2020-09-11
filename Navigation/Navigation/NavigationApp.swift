5adad60e322b2a6996f83421be154e9d2d11b7b9
Eugene Berezin <berezin.nlp@gmail.com>
2020-09-10 18:38:33 -0700

    finsihed

---
+++ Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Eugene Berezin on 9/7/20.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
